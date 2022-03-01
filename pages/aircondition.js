d0e9eb23442910da6f21c19a2bfa2dcc68d8ffc6
hanzhenfang <hanzhenfffang@gmail.com>
2022-03-01 20:33:35 +0800

    个人博客

---
+++ pages/aircondition.js
@@ -0,0 +1,12 @@
+
+import Link from "next/link"
+
+function Aircondition() {
+    return (
+        <>
+            <h1>先别着急,天气这么热,先打开空调吧～</h1>
+            <Link href="/"><a>返回首页</a></Link>
+        </>
+    )
+}
+export default Aircondition
